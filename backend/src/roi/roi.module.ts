fcb440f2fb000fbea672ca3565504fb76aed823b
Harsh Patil <harshpatil53342@gmail.com>
2026-09-04 16:16:17 +0530

    fix: provide auth dependencies to roi module

--- backend/src/roi/roi.module.ts
+++ backend/src/roi/roi.module.ts
@@ -1,11 +1,15 @@
 ﻿import { Module } from '@nestjs/common';
 
+import { AuthModule } from '../auth/auth.module';
 import { PrismaModule } from '../prisma/prisma.module';
 import { RoiController } from './roi.controller';
 import { RoiService } from './roi.service';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    AuthModule,
+  ],
   controllers: [RoiController],
   providers: [RoiService],
   exports: [RoiService],
