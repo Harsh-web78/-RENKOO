35adc3a8d94799af4a3f12b3fb17f56386270de6
Harsh Patil <harshpatil53342@gmail.com>
2026-09-04 16:35:01 +0530

    fix: make auth dependencies globally available

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+﻿import { Global, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 
 import { PrismaModule } from '../prisma/prisma.module';
@@ -6,24 +6,21 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './jwt-auth.guard';
 
+@Global()
 @Module({
   imports: [
     PrismaModule,
-
     JwtModule.register({
       secret: process.env.JWT_ACCESS_SECRET,
     }),
   ],
-
   controllers: [
     AuthController,
   ],
-
   providers: [
     AuthService,
     JwtAuthGuard,
   ],
-
   exports: [
     AuthService,
     JwtAuthGuard,
